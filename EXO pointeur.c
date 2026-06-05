46c084141220cf7493778402e24f35ce1afb85e4
Mayapbreedlove <mayapbreedlove@gmail.com>
2026-06-06 09:00:28 +1000

    Add files via upload

---
+++ EXO pointeur.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void sommesmatrices (int (*matrice1)[2],(*matrice2) [2],(*resultat) [2]") {
+for(int i=0,j<1,i++){
+        for(int j=0,j<1,j++){
+            *(*resultat+i)+j)=*((*matrice1+i)+j) +*((*matrice1+i)+j);
+        }
+}
+    }
